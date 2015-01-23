e44dbdc099079de01b6dc09670343bc58ea422ba
solenee <se72wuny@clientmaster.rbg.informatik.tu-darmstadt.de>
2015-01-23 16:31:14 +0100

    problem2 : concatenate_features.m implemented (to be tested)

--- problem2/concatenate_features.m
+++ problem2/concatenate_features.m
@@ -8,8 +8,17 @@ function X = concatenate_features(features)
 %   X                 data matrix of size 128xN, where N is the number of all interest points
 %                     in all images.
 %%
+nbImages = length(features);
+X = [];
+for j=1:nbImages
+    N_p = size(X,2);
+    feature_matrix = features{j};
+    M = size(feature_matrix, 2);
+    for i=N_p+1:N_p+M
+        X(:,i) = feature_matrix(i-N_p);
+    end
+end
 
-X =
 
 % format check
 assert(size(X,1) == 128);
